23f20f3818f99decd08f3d2f20206b4059962f9b
wms2537 <swmeng@yes.my>
2023-07-24 14:29:26 +0800

    store objects

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -3,10 +3,12 @@ package types
 // DONTCOVER
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	sdkerrors "cosmossdk.io/errors"
 )
 
 // x/checkers module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
+	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
+	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
 )
